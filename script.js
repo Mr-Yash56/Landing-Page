a3c7ccab66b902a6ff3a4cc41212b5c4f42921d9
Tallapalli Yeswanth Kumar <yaswanth56@icloud.com>
2025-03-22 17:24:17 +0530

    Task-1 Landing-Page

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle Mobile Menu
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+}
